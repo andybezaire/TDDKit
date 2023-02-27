e74e17e36aacb213d7aff3ef4fecbeaa4d31de36
andy <andy.bezaire@gmail.com>
2023-02-27 08:39:31 +0200

    Add expect will deallocate instance

---
+++ Sources/TestHelpers/ExpectWillDeallocateInstance.swift
@@ -0,0 +1,49 @@
+import XCTest
+
+extension XCTestCase {
+    /// Fail a test if the instance was not deallocted.
+    ///
+    /// Memory leaks occur when an object is not properly released from memory.
+    /// This often happens when mistakenly creating a circular strong reference.
+    ///
+    /// This method will help to catch these kinds of errors by making sure
+    /// that your object has been deallocated by the end of the test.
+    /// It is most useful to use it in your makeSUT method.
+    ///
+    /// - Note: A test may finish without deallocating an object.
+    /// This is especially likely to occur in tests involving asyncronity.
+    /// It is recommended that you track all objects created for a test and make sure to
+    /// wait for all long lived processes to finish before the test ends.
+    ///
+    /// Usage:
+    ///
+    /// ```swift
+    /// private func makeSUT(
+    ///     userLoginResult: Result<Void, Error> = .success(()),
+    ///     file: StaticString = #file,
+    ///     line: UInt = #line
+    /// ) -> (sut: LoginFlow, spy: Spy) {
+    ///
+    ///     let spy = Spy(userLoginResult: userLoginResult)
+    ///     let sut = LoginFlow(service: spy)
+    ///
+    ///     expectWillDeallocate(for: sut, file: file, line: line)
+    ///     expectWillDeallocate(for: spy, file: file, line: line)
+    ///
+    ///     return (sut, spy)
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - instance: The object that is expected to be deallocted.
+    ///   - file: The file where the failure occurs.
+    ///   The default is the filename of the test case where you call this function.
+    ///   - line: The line number where the failure occurs.
+    ///   The default is the line number where you call this function.
+    func expectWillDeallocate(instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            let description = String(describing: instance)
+            XCTAssertNil(instance, "Should have been deallocated. Possible memory leak.", file: file, line: line)
+        }
+    }
+}
---
+++ Tests/TestHelpersTests/ExpectWillDeallocateInstanceTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import TestHelpers
+
+final class ExpectWillDeallocateInstanceTests: XCTestCase {
+    func test_circularReference_example() throws {
+        let spy = Instance()
+        let sut = Instance(reference: spy)
+        spy.reference = sut
+
+        XCTExpectFailure()
+
+        expectWillDeallocate(instance: sut)
+        expectWillDeallocate(instance: spy)
+    }
+
+    func test_selfReferencing_example() throws {
+        let sut = Instance()
+        sut.reference = sut
+
+        XCTExpectFailure()
+
+        expectWillDeallocate(instance: sut)
+    }
+
+    private class Instance {
+        var reference: Instance?
+        init(reference: Instance? = nil) { self.reference = reference }
+    }
+}
