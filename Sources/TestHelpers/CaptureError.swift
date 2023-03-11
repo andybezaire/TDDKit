b2b0541f232bd8e4512fe8140da884113146d648
andy <andy.bezaire@gmail.com>
2023-03-11 10:31:02 +0200

    Add documentation

--- Sources/TestHelpers/CaptureError.swift
+++ Sources/TestHelpers/CaptureError.swift
@@ -1,6 +1,23 @@
 import XCTest
 
 public extension XCTestCase {
+    /// Helper to extract the error from an asyncronous throwing function call.
+    ///
+    /// This helps reduce boilerplate code when writing error tests for TDD.
+    /// For example:
+    /// ```
+    /// func test_failingFetchX_fetchY_fails() async throws {
+    ///     let error = AnyError()
+    ///     let (sut, _) = makeSUT(fetchXResult: .failure(error))
+    ///
+    ///     let capturedError = await captureError(from: try await sut.fetchY())
+    ///
+    ///     XCTAssertNotNil(error)
+    ///     XCTAssertEqual(capturedError as? AnyError, error)
+    /// }
+    /// ```
+    /// - Parameter block: The function call under test.
+    /// - Returns: The error thrown from the block or nil if no error thrown.
      func captureError<T>(from block: @autoclosure () async throws -> T) async -> Error? {
         var capturedError: Error?
         do {
