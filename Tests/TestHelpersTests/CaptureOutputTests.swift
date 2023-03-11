a0af5ccd7be779c9aea370f9ecd607e7a80853dc
andy <andy.bezaire@gmail.com>
2023-03-11 22:23:12 +0200

    Move to production

--- Tests/TestHelpersTests/CaptureOutputTests.swift
+++ Tests/TestHelpersTests/CaptureOutputTests.swift
@@ -61,19 +61,6 @@ final class CaptureOutputTests: XCTestCase {
     }
 }
 
-import Combine
-
-private func captureOutput<P, O>(for publisher: P, droppingFirst dropCount: Int = 1, when block: () async -> Void) async -> [O] where P: Publisher, P.Failure == Never, P.Output == O {
-    var capturedOutput: [O] = []
-    let publishing = publisher
-        .dropFirst(dropCount)
-        .sink { capturedOutput.append($0) }
-    
-    await block()
-    publishing.cancel()
-    return capturedOutput
-}
-
 // MARK: - production
 private protocol TitleService {
     func fetchTitle() async throws -> String
