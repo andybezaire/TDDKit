b929a4aa679b4b0670fbc688e63786aeff2f4dc7
andy <andy.bezaire@gmail.com>
2023-03-11 22:23:44 +0200

    Refactor for clarity

--- Sources/TestHelpers/CaptureOutput.swift
+++ Sources/TestHelpers/CaptureOutput.swift
@@ -1,7 +1,12 @@
 import Combine
 
-public func captureOutput<P, O>(for publisher: P, droppingFirst dropCount: Int = 1, when block: () async -> Void) async -> [O] where P: Publisher, P.Failure == Never, P.Output == O {
+public func captureOutput<P, O>(
+    for publisher: P,
+    droppingFirst dropCount: Int = 1,
+    when block: () async -> Void
+) async -> [O] where P: Publisher, P.Failure == Never, P.Output == O {
     var capturedOutput: [O] = []
+    
     let publishing = publisher
         .dropFirst(dropCount)
         .sink { capturedOutput.append($0) }
