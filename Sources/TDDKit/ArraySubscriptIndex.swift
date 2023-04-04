2452a5198189398a7ee83092b208c7c754e6bfd9
andy <andy.bezaire@gmail.com>
2023-04-04 21:57:39 +0300

    Improve failure message

--- Sources/TDDKit/ArraySubscriptIndex.swift
+++ Sources/TDDKit/ArraySubscriptIndex.swift
@@ -3,7 +3,7 @@ import XCTest
 extension Array {
     public subscript(index index: Index, file: StaticString = #file, line: UInt = #line) -> Element? {
         guard indices.contains(index) else {
-            XCTFail("Index \(index) out of range \(startIndex) ..< \(endIndex).", file: file, line: line)
+            XCTFail("index \(index) out of range \(startIndex) ..< \(endIndex).", file: file, line: line)
             return nil
         }
 
