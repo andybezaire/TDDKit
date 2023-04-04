8506564305dc6eb5076808d70607828e16f1ccad
andy <andy.bezaire@gmail.com>
2023-04-04 21:36:23 +0300

    Put in test case scope

--- Sources/TDDKit/AnyError.swift
+++ Sources/TDDKit/AnyError.swift
@@ -1,24 +1,27 @@
 import Foundation
+import XCTest
 
-/// An equatable identifiable error.
-///
-/// This can be used to test that a service throws the error passed to one of its dependencies.
-/// For example:
-/// ```
-/// func test_failingFetchX_fetchY_fails() async throws {
-///     let error = AnyError()
-///     let (sut, _) = makeSUT(fetchXResult: .failure(error))
-///
-///     let capturedError = await XCTCaptureError(from: try await sut.fetchY())
-///
-///     XCTAssertNotNil(error)
-///     XCTAssertEqual(capturedError as? AnyError, error)
-/// }
-/// ```
-public struct AnyError: Error, Equatable, Identifiable {
-    public let id: UUID
+public extension XCTestCase {
+    /// An equatable identifiable error.
+    ///
+    /// This can be used to test that a service throws the error passed to one of its dependencies.
+    /// For example:
+    /// ```
+    /// func test_failingFetchX_fetchY_fails() async throws {
+    ///     let error = AnyError()
+    ///     let (sut, _) = makeSUT(fetchXResult: .failure(error))
+    ///
+    ///     let capturedError = await XCTCaptureError(from: try await sut.fetchY())
+    ///
+    ///     XCTAssertNotNil(error)
+    ///     XCTAssertEqual(capturedError as? AnyError, error)
+    /// }
+    /// ```
+    struct AnyError: Error, Equatable, Identifiable {
+        public let id: UUID
 
-    public init(id: UUID = .init()) {
-        self.id = id
+        public init(id: UUID = .init()) {
+            self.id = id
+        }
     }
 }
