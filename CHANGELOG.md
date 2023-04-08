14a996fd133aa738e06a78bc978645712249e897
andy <andy.bezaire@gmail.com>
2023-04-08 21:05:36 +0300

    Add documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 ### Added
+- Assert Contains Equal
 ### Fixed
 ### Changed
 - Renamed for a nicer API
--- Sources/TDDKit/XCTAssertContainsEqual.swift
+++ Sources/TDDKit/XCTAssertContainsEqual.swift
@@ -1,6 +1,28 @@
 import XCTest
 
 public extension XCTestCase {
+    /// Asserts that the first collection exactly contains all the elements of the second collection.
+    /// Fails with a different message for missing elements, extra elements, or both.
+    /// This is an order independent compare.
+    ///
+    /// Use this function to compare two collections to easily see if they contain exactly the same elements.
+    /// For example:
+    /// ```
+    /// func test_createPoem_callsServices() async throws {
+    ///     let (sut, spy) = makeSUT()
+    ///
+    ///     _ = try await sut.createPoem()
+    ///
+    ///     XCTAssertContainsEqual(spy.messages, [.getAge, .getUsername])
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - expression1: An expression of type `T`, where `T` is a `Collection`  whose `Element`s are `Equatable`.
+    ///   - expression2: A second expression of type `T`, where `T` is a `Collection`  whose `Element`s are `Equatable`.
+    ///   - message: An optional description of a failure.
+    ///   - file: The file where the failure occurs. The default is the filename of the test case where you call this function.
+    ///   - line: The line number where the failure occurs. The default is the line number where you call this function.
     func XCTAssertContainsEqual<T>(
         _ expression1: @autoclosure () throws -> T,
         _ expression2: @autoclosure () throws -> T,
