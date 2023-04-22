1a19003929bf174aa5dc91ce53e2b7cf4bbe36ef
andy <andy.bezaire@gmail.com>
2023-04-22 11:30:58 +0300

    Add complex enums functionality

--- Tests/TDDKitTests/XCTCustomStringConvertibleTests.swift
+++ Tests/TDDKitTests/XCTCustomStringConvertibleTests.swift
@@ -23,13 +23,72 @@ final class XCTCustomStringConvertibleTests: XCTestCase {
     func test_simpleEnumsAssert_description_succeeds() {
         let caseOne = Simple.one
         let caseTwo = Simple.two
-        let caseThree = Simple.three
 
         XCTExpectFailure {
-            XCTAssertEqual(Simple.one, .two)
-            XCTAssertCountEqual([Simple.one], [.two])
-            XCTAssertContainsEqual([Simple.one], [.two])
-            XCTAssertCastEqual(Simple.one, Simple.two)
+            XCTAssertEqual(caseOne, caseTwo)
+            XCTAssertCountEqual([caseOne], [caseTwo])
+            XCTAssertContainsEqual([caseOne], [caseTwo])
+            XCTAssertCastEqual(caseOne, caseTwo)
+        }
+    }
+
+    func test_complexEnums_description_succeeds() {
+        let caseFirst = Complex.first
+        let caseSecond = Complex.second("Second case")
+        let caseThird = Complex.third(34, "Number and string")
+        let caseFourth = Complex.fourth
+        let caseFifth = Complex.fifth(45)
+        let caseSecondNil = Complex.second(nil)
+
+        XCTAssertEqual("\(caseFirst)", ".first")
+        XCTAssertEqual("\(caseSecond)", #".second(Optional("Second case"))"#)
+        XCTAssertEqual("\(caseThird)", #".third(34, "Number and string")"#)
+        XCTAssertEqual("\(caseFourth)", ".fourth")
+        XCTAssertEqual("\(caseFifth)", ".fifth(45)")
+        XCTAssertEqual("\(caseSecondNil)", #".second(nil)"#)
+    }
+
+    func test_complexEnumsAssert_description_succeeds() {
+        let caseFirst = Complex.first
+        let caseSecond = Complex.second("Second case")
+        let caseThird = Complex.third(34, "Number and string")
+        let caseFourth = Complex.fourth
+        let caseFifth = Complex.fifth(45)
+        let caseSecondNil = Complex.second(nil)
+
+        XCTExpectFailure {
+            XCTAssertEqual(caseFirst, caseFourth)
+            XCTAssertCountEqual([caseFirst], [caseFourth])
+            XCTAssertContainsEqual([caseFirst], [caseFourth])
+            XCTAssertCastEqual(caseFirst, caseFourth)
+        }
+
+        XCTExpectFailure {
+            XCTAssertEqual(caseSecond, caseFourth)
+            XCTAssertCountEqual([caseSecond], [caseFourth])
+            XCTAssertContainsEqual([caseSecond], [caseFourth])
+            XCTAssertCastEqual(caseSecond, caseFourth)
+        }
+
+        XCTExpectFailure {
+            XCTAssertEqual(caseThird, caseFourth)
+            XCTAssertCountEqual([caseThird], [caseFourth])
+            XCTAssertContainsEqual([caseThird], [caseFourth])
+            XCTAssertCastEqual(caseThird, caseFourth)
+        }
+
+        XCTExpectFailure {
+            XCTAssertEqual(caseSecond, caseThird)
+            XCTAssertCountEqual([caseSecond], [caseThird])
+            XCTAssertContainsEqual([caseSecond], [caseThird])
+            XCTAssertCastEqual(caseSecond, caseThird)
+        }
+
+        XCTExpectFailure {
+            XCTAssertEqual(caseSecondNil, caseFifth)
+            XCTAssertCountEqual([caseSecondNil], [caseFifth])
+            XCTAssertContainsEqual([caseSecondNil], [caseFifth])
+            XCTAssertCastEqual(caseSecondNil, caseFifth)
         }
     }
 
@@ -41,6 +100,7 @@ final class XCTCustomStringConvertibleTests: XCTestCase {
 
 extension Instance: XCTCustomStringConvertible { }
 extension Simple: XCTCustomStringConvertible { }
+extension Complex: XCTCustomStringConvertible { }
 
 // MARK: - production
 private protocol Port { }
@@ -52,14 +112,33 @@ private class Instance: Suting {
     }
 }
 
-private enum Simple: CaseIterable { case one, two, three }
+private enum Simple { case one, two, three }
+private enum Complex: Equatable { case first, second(String?), third(Int, String), fourth, fifth(Int) }
 
 // MARK: - move to production
 protocol XCTCustomStringConvertible: CustomStringConvertible { }
 extension XCTCustomStringConvertible {
     var description: String {
         if let caseName = getEnumCaseName(for: self) {
-            return ".\(caseName)"
+            let reflection = Mirror(reflecting: self)
+
+            guard reflection.displayStyle == .enum,
+                  let associated = reflection.children.first else { return ".\(caseName)" }
+
+            let value: String
+            switch associated.value {
+            case is String?:
+                value = "\(associated.value)"
+            case is String:
+                value = "\"\(associated.value)\""
+            default:
+                value = "\(associated.value)"
+            }
+
+            let isBracketted = value.first == "(" && value.last == ")"
+            let associatedValues = isBracketted ? String(value.dropFirst().dropLast(1)) : value
+
+            return ".\(caseName)(\(associatedValues))"
         } else {
             let reflection = Mirror(reflecting: self)
             return "\(reflection.subjectType)"
